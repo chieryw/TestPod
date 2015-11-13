5287ab094a69905528156fa2d47000687ad4b04e
handong.wang <handong.wang@qunar.com>
2015-11-13 16:59:52 +0800

    测试Pod

---
+++ TestPod/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestPod
+//
+//  Created by chiery on 15/11/13.
+//  Copyright © 2015年 My-Zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
